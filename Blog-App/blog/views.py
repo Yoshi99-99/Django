d9b1b811d1727f22c2621eff9cacbcb507b41dac
Yoshi99-99 <j.yoshi2002@gmail.com>
2023-02-19 05:22:21 +0900

    blog

---
+++ Blog-App/blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
